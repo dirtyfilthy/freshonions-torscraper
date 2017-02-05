54a38668eaf915b86eed82798667b4880a6afb9f
Caleb Anderson <dirtyfilthee@gmail.com>
2017-02-05 19:55:38 +1100

    fix wesgi.py

--- web/wsgi.py
+++ web/wsgi.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.insert(0, '/home/alhazred/torscraper/lib/')
-sys.path.insert(0, '/home/alhazred/torscraper/web/')
+sys.path.insert(0, '/home/alhazred/freshonions/lib/')
+sys.path.insert(0, '/home/alhazred/freshonions/web/')
 from app import app
