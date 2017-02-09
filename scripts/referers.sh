0ce898cd5be6bbd0163d32efdcd067bf55062940
Caleb <caleb.anderson@contextis.com>
2017-02-09 12:05:42 +1100

    remove self from referers

--- scripts/referers.sh
+++ scripts/referers.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /var/log/nginx/access.log | cut -f 4 -d '"' | grep -E -v '^-' | sort | uniq
+cat /var/log/nginx/access.log | cut -f 4 -d '"' | grep -E -v '^-'  | grep -v "zlal32teyptf4tvi.onion" | sort | uniq
