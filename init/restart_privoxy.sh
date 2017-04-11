1c919ce39c2d29e087050918f0e1d1da7f4769b5
Caleb Anderson <dirtyfilthee@gmail.com>
2017-04-12 01:04:21 +1000

    fix restart privoxy

--- init/restart_privoxy.sh
+++ init/restart_privoxy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-/usr/bin/service privoxy1 restart
-/usr/bin/service privoxy2 restart
-/usr/bin/service privoxy3 restart
-/usr/bin/service privoxy4 restart
-/usr/bin/service privoxy5 restart
-/usr/bin/service privoxy6 restart
-/usr/bin/service privoxy7 restart
-/usr/bin/service privoxy8 restart
+/usr/sbin/service privoxy1 restart
+/usr/sbin/service privoxy2 restart
+/usr/sbin/service privoxy3 restart
+/usr/sbin/service privoxy4 restart
+/usr/sbin/service privoxy5 restart
+/usr/sbin/service privoxy6 restart
+/usr/sbin/service privoxy7 restart
+/usr/sbin/service privoxy8 restart
