02fb81b3f2b1ea39e43db493fd8f901543e9a4c0
Caleb Anderson <dirtyfilthee@gmail.com>
2017-06-27 01:37:00 +1000

    increase penalty more

--- torscraper/spiders/tor_scrapy.py
+++ torscraper/spiders/tor_scrapy.py
@@ -17,9 +17,9 @@ import email_util
 import interesting_paths
 import tor_text
 
-SUBDOMAIN_PENALTY    = 4 * 60
+SUBDOMAIN_PENALTY    = 6 * 60
 NORMAL_RAND_RANGE    = 2 * 60
-SUBDOMAIN_RAND_RANGE = 4 * 60
+SUBDOMAIN_RAND_RANGE = 6 * 60
 
 from scrapy.exceptions import IgnoreRequest
 
